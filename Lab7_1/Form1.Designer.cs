4e48b83e6c0ce8104452820b164b98761084f9f7
Баринская Ольга <114239444+barinskaya@users.noreply.github.com>
2023-02-25 14:01:56 +0300

    Add files via upload

---
+++ Lab7_1/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace Lab7_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pct = new System.Windows.Forms.PictureBox();
+            this.btn = new System.Windows.Forms.Button();
+            this.file1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.save1 = new System.Windows.Forms.SaveFileDialog();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pct
+            // 
+            this.pct.Location = new System.Drawing.Point(60, 25);
+            this.pct.Name = "pct";
+            this.pct.Size = new System.Drawing.Size(200, 150);
+            this.pct.TabIndex = 0;
+            this.pct.TabStop = false;
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(11, 237);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(128, 23);
+            this.btn.TabIndex = 1;
+            this.btn.Text = "Загрузить рисунок";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // file1
+            // 
+            this.file1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(102, 198);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(192, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 201);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Имя рисунка";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(180, 237);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(129, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Сохранить рисунок";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(322, 271);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.pct);
+            this.Name = "Form1";
+            this.Text = "Просмотр графического файла";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pct)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pct;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.OpenFileDialog file1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SaveFileDialog save1;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
